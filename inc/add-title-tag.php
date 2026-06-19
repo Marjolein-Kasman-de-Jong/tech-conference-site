e17f741f12fa82dbbb24f3e475b81528c69290b8
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2026-06-19 16:30:16 +0200

    Fix title tag content

--- inc/add-title-tag.php
+++ inc/add-title-tag.php
@@ -5,3 +5,29 @@ function add_title_tag() {
 }
 
 add_action('after_setup_theme', 'add_title_tag');
+
+
+function get_document_title($title) {
+    if (is_front_page()) {
+        return get_bloginfo('name');
+    }
+
+    if (is_post_type_archive()) {
+        $post_type = get_query_var('post_type');
+
+        if (is_array($post_type)) {
+            $post_type = reset($post_type);
+        }
+
+        $archive_titles = [
+            'event' => 'Schedule',
+            'speaker' => 'Speakers',
+        ];
+
+        return $archive_titles[$post_type] ?? ucfirst($post_type);
+    }
+
+    return $title;
+}
+
+add_filter('pre_get_document_title', 'get_document_title');
