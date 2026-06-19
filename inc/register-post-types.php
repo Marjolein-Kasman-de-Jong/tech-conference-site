c464a415fb0988d69027b62114c2a23686d67848
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2026-06-19 16:03:27 +0200

    Add breakpoints, variables, typography

--- inc/register-post-types.php
+++ inc/register-post-types.php
@@ -3,7 +3,18 @@
 function register_post_types() {
     register_post_type('event', [
         'public' => true,
-        'label' => 'Events',
+        'labels' => [
+            'name' => 'Events',
+            'singular_name' => 'Event',
+            'add_new' => 'Nieuw event',
+            'add_new_item' => 'Nieuw event toevoegen',
+            'edit_item' => 'Event bewerken',
+            'new_item' => 'Nieuw event',
+            'view_item' => 'Event bekijken',
+            'search_items' => 'Events zoeken',
+            'not_found' => 'Geen events gevonden',
+            'all_items' => 'Alle events',
+        ],
         'has_archive' => true,
         'rewrite' => [
             'slug' => 'schedule',
@@ -14,7 +25,18 @@ function register_post_types() {
 
     register_post_type('speaker', [
         'public' => true,
-        'label' => 'Speakers',
+        'labels' => [
+            'name' => 'Speakers',
+            'singular_name' => 'Speaker',
+            'add_new' => 'Nieuwe speaker',
+            'add_new_item' => 'Nieuwe speaker toevoegen',
+            'edit_item' => 'Speaker bewerken',
+            'new_item' => 'Nieuwe speaker',
+            'view_item' => 'Speaker bekijken',
+            'search_items' => 'Speakers zoeken',
+            'not_found' => 'Geen speakers gevonden',
+            'all_items' => 'Alle speakers',
+        ],
         'has_archive' => true,
         'rewrite' => [
             'slug' => 'speakers',
