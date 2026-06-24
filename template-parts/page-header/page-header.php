4335e922feef7adc2f9e340530bb123360251eb9
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2026-06-24 14:56:55 +0200

    Enhance code readability

--- template-parts/page-header/page-header.php
+++ template-parts/page-header/page-header.php
@@ -1,14 +1,17 @@
+<?php
+// Image folder path
+$images_uri = get_template_directory_uri() . '/assets/images';
+?>
+
 <header class="page-header">
     <div class="content-wrapper">
-        <img class="logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="">
+        <img class="logo" src="<?php echo $images_uri; ?>/logo.svg" alt="">
         <div class="menu-wrapper menu-closed">
             <button id="btn-menu-open" class="btn-menu" type="button">
-                <img class="menu-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-menu.svg"
-                    alt="">
+                <img class="menu-icon" src="<?php echo $images_uri; ?>/icon-menu.svg" alt="">
             </button>
             <button id="btn-menu-close" class="btn-menu" type="button">
-                <img class="close-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-cross.svg"
-                    alt="">
+                <img class="close-icon" src="<?php echo $images_uri; ?>/icon-cross.svg" alt="">
             </button>
 
             <nav class="page-header-nav" aria-label="Hoofdmenu">
